25873d337e17814d65636343957e829a382ecc04
Rajesh Anuragi <rajeshanuragi2003@gmail.com>
2026-07-27 13:36:44 +0530

    cors erroe resloved

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://lead-management-omega-cyan.vercel.app"
+        process.env.FRONTEND_URL
     ],
     credentials: true
 }));
